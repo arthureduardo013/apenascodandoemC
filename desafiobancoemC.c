8da6f831262834a5223fdd74b987ad93f1bc88aa
arthureduardo013 <202601323482@alunos.estacio.br>
2026-06-06 02:06:44 -0300

    feature: atualização.

--- desafiobancoemC.c
+++ desafiobancoemC.c
@@ -1,52 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main() {
 
     int saldo = 100;
-    int opção;
+    int opcao;
 
     do {
-    system("cls");
-    printf("==Banco Intergalatico==");
-    printf("\n");
-    printf("Opções dispónivel:");
-    printf("\n");
-    printf("1.Para depositar 50 créditos.");
-    printf("\n");
-    printf("2.Para sacar 20 créditos.");
-    printf("\n");
-    printf("0.Para sair do Banco");
-    scanf(" %d", &opção);
 
-    switch (opção)
-    {
-    case 1: (saldo = saldo + 50);
-        printf("depósito realizado valor em saldo : %d", saldo);
-        break;
-    case 2: if (saldo >= 20) {
-        saldo = saldo - 20;
-        printf("saldo na conta após saque %d", saldo);
-    } else {
-        printf("você não tem saldo o suficiente");
-    }
-        break;
-    case 0: 
-        printf("Saindo do Banco obrigado pela preferência");
-        break;
-    default:
-    printf("opção invalida tente novamente");
-        break;
-    }
-    if (opção != 0) {
-        printf("pressione enter para continuar");
-        fflush(stdin);
-        getchar();
-    }
-    
-   
-    } while (opção != 0);
-    
-     return 0;
-    
+        system("cls");
+
+        printf("== Banco Intergalatico ==\n");
+        printf("1. Depositar 50 creditos\n");
+        printf("2. Sacar 20 creditos\n");
+        printf("0. Sair\n");
+        printf("Escolha: ");
+
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+
+        case 1:
+            saldo += 50;
+            printf("Deposito realizado. Saldo: %d\n", saldo);
+            break;
+
+        case 2:
+            if (saldo >= 20) {
+                saldo -= 20;
+                printf("Saque realizado. Saldo: %d\n", saldo);
+            } else {
+                printf("Saldo insuficiente!\n");
+            }
+            break;
+
+        case 0:
+            printf("Saindo do banco. Obrigado pela preferencia!\n");
+            break;
+
+        default:
+            printf("Opcao invalida!\n");
+        }
+
+        if (opcao != 0) {
+            printf("\nPressione Enter para continuar...");
+            getchar();
+            getchar();
+        }
+
+    } while (opcao != 0);
+
+    return 0;
 }
